6ecb5841f76fe71f5e2d048e2f3f56eb7b315c38
Byaxy <charlesbyaxy@gmail.com>
2022-12-22 11:45:03 +0000

    update

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
         <h1>Counter App</h1>
       </header>
       <section className="main-section">
-        <h2>{count}</h2>
-        <button id="increment" onClick={() => setCount(count + 1)}>
-          Increment
-        </button>
+        <h2 className={count < 0 ? "red" : ""}>{count}</h2>
         <button id="decrement" onClick={() => setCount(count - 1)}>
           Decrement
+        </button>
+        <button id="increment" onClick={() => setCount(count + 1)}>
+          Increment
         </button>
         <button id="reset" onClick={() => setCount(0)}>
           Reset
